ebbeec7412e606b9571e8f1a59a60a880875c627
Basilio Vera <basilio.vera@softonic.com>
2016-10-07 17:13:12 +0200

    Show existing jenkins home content when starting the container

--- jenkins-restore-backup.sh
+++ jenkins-restore-backup.sh
@@ -8,6 +8,9 @@ if [ -d "$JENKINS_HOME_BACKUP_DIR" ] && [ "$(ls -A $JENKINS_HOME_BACKUP_DIR)" ];
     echo "Content in backup dir '$JENKINS_HOME_BACKUP_DIR' copied to home dir '$JENKINS_HOME'"
   else
     echo "Backup found but existing data in jenkins home won't be overwritten"
+    set -ex
+    ls -la $JENKINS_HOME
+    set +ex
   fi
 else
   echo "No jenkins_home backup found"
