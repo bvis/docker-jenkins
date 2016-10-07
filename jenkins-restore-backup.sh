48148c105f876077427f867d7241e6f935cb35e5
Basilio Vera <basilio.vera@softonic.com>
2016-10-07 17:39:44 +0200

    Fix ls of the jenkins_home dir

--- jenkins-restore-backup.sh
+++ jenkins-restore-backup.sh
@@ -3,13 +3,13 @@
 JENKINS_HOME=/var/jenkins_home
 
 if [ -d "$JENKINS_HOME_BACKUP_DIR" ] && [ "$(ls -A $JENKINS_HOME_BACKUP_DIR)" ]; then
-  if [ ! -d "$JENKINS_HOME" ] || [ ! "$(ls -A $JENKINS_HOME)" ]; then
+  if [ ! -d "$JENKINS_HOME" ] || [ ! "$(ls -lsA --ignore='.*' $JENKINS_HOME)" ]; then
     cp -r $JENKINS_HOME_BACKUP_DIR/* $JENKINS_HOME
     echo "Content in backup dir '$JENKINS_HOME_BACKUP_DIR' copied to home dir '$JENKINS_HOME'"
   else
     echo "Backup found but existing data in jenkins home won't be overwritten"
     set -ex
-    ls -la $JENKINS_HOME
+    ls -lsA --ignore='.*' $JENKINS_HOME
     set +ex
   fi
 else
